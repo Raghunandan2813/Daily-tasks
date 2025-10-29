3051db39dde3c7787166b071e2dde280984e8d2f
Raghunandan <raghu9555k@gmail.com>
2025-10-29 21:52:28 +0530

    added video page

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -2,15 +2,64 @@ import React from 'react'
 import './Feed.css';
 import thumbnail3 from '../../assets/thumbnail3.png'
 import thumbnail4 from '../../assets/thumbnail4.png'
-import thumbnail5 from '../../assets/thumbnail4.png'
-import thumbnail6 from '../../assets/thumbnail4.png'
-import thumbnail7 from '../../assets/thumbnail4.png'
-import thumbnail8 from '../../assets/thumbnail4.png'
-import thumbnail1 from '../../assets/thumbnail4.png'
-import thumbnail2 from '../../assets/thumbnail4.png'
+import thumbnail5 from '../../assets/thumbnail5.png'
+import thumbnail6 from '../../assets/thumbnail6.png'
+import thumbnail7 from '../../assets/thumbnail7.png'
+import thumbnail8 from '../../assets/thumbnail8.png'
+import thumbnail1 from '../../assets/thumbnail1.png'
+import thumbnail2 from '../../assets/thumbnail2.png'
+import { Link } from 'react-router-dom';
 const Feed = () => {
   return (
     <div className='feed'>
+    <Link to={`video/20/4521`} className='card'>
+      <img src={thumbnail1}/>
+      <h2>Best channel to learning coding that help you to be a web developer</h2>
+      <h3>Learning</h3>
+      <p>10k views &bull; 3 days ago</p>
+     </Link>
+    <div className='card'>
+      <img src={thumbnail2}/>
+      <h2>Best channel to learning coding that help you to be a web developer</h2>
+      <h3>Learning</h3>
+      <p>10k views &bull; 3 days ago</p>
+     </div>
+    <div className='card'>
+      <img src={thumbnail3}/>
+      <h2>Best channel to learning coding that help you to be a web developer</h2>
+      <h3>Learning</h3>
+      <p>10k views &bull; 3 days ago</p>
+     </div>
+    <div className='card'>
+      <img src={thumbnail4}/>
+      <h2>Best channel to learning coding that help you to be a web developer</h2>
+      <h3>Learning</h3>
+      <p>10k views &bull; 3 days ago</p>
+     </div>
+    <div className='card'>
+      <img src={thumbnail5}/>
+      <h2>Best channel to learning coding that help you to be a web developer</h2>
+      <h3>Learning</h3>
+      <p>10k views &bull; 3 days ago</p>
+     </div>
+    <div className='card'>
+      <img src={thumbnail6}/>
+      <h2>Best channel to learning coding that help you to be a web developer</h2>
+      <h3>Learning</h3>
+      <p>10k views &bull; 3 days ago</p>
+     </div>
+    <div className='card'>
+      <img src={thumbnail7}/>
+      <h2>Best channel to learning coding that help you to be a web developer</h2>
+      <h3>Learning</h3>
+      <p>10k views &bull; 3 days ago</p>
+     </div>
+    <div className='card'>
+      <img src={thumbnail8}/>
+      <h2>Best channel to learning coding that help you to be a web developer</h2>
+      <h3>Learning</h3>
+      <p>10k views &bull; 3 days ago</p>
+     </div>
     <div className='card'>
       <img src={thumbnail1}/>
       <h2>Best channel to learning coding that help you to be a web developer</h2>
